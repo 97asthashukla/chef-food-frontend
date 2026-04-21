6fecff57efde32de794ee897a3a8055976ddd24e
97asthashukla <97asthashukla@gmail.com>
2026-04-21 20:48:18 +0530

    Add server.js and update constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,10 +2,12 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeVpeqdR9OxtpTOdBuyf7npN4dQ5BWRQWhOcsRMxVxojE9Eaf2mXDl_KM&s"
 
-export const MENU_URL = "https://corsproxy.io/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5822513&lng=77.3334716&restaurantId="
+export const API_BASE_URL = "http://localhost:5000/api";
+
+export const MENU_URL = `${API_BASE_URL}/menu/`
 
 // export const RESTAURANT_LIST_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5822513&lng=77.3334716&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-export const FALLBACK_IMAGE = "https://via.placeholder.com/300x200?text=No+Image";4
+export const FALLBACK_IMAGE = "https://via.placeholder.com/300x200?text=No+Image";
 
-export const RESTAURANT_LIST_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5822513&lng=77.3334716&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANT_LIST_URL = `${API_BASE_URL}/restaurants/list`
